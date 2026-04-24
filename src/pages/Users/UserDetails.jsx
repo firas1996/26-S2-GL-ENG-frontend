5758a58de1e21dfcb7df23d9f1ad034da4def740
firas1996 <firas.hm.1996@gmail.com>
2026-04-24 11:36:45 +0100

    aa

--- src/pages/Users/UserDetails.jsx
+++ src/pages/Users/UserDetails.jsx
@@ -3,19 +3,18 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const UserDetails = () => {
-  const params = useParams();
+  const { id } = useParams();
   const [user, setUser] = useState({});
-  console.log(params);
   useEffect(() => {
     axios
-      .get(`http://localhost:1234/users/${params.id}`)
+      .get(`http://localhost:1234/users/${id}`)
       .then((res) => {
         setUser(res.data.data.user);
       })
       .catch((e) => {
         console.log(e);
       });
-  }, []);
+  }, [id]);
   return (
     <div>
       <h1>User Name: {user.name} </h1>
